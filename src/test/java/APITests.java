c77e9a08edc446b2a2d5985df1b52cfa434f1462
ILYA <ilvolkov1@gmail.com>
2024-01-08 23:50:57 +0400

    created more tests for different endpoints

--- src/test/java/APITests.java
+++ src/test/java/APITests.java
@@ -1,11 +1,15 @@
-import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import com.github.javafaker.Faker;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.*;
 
 public class APITests {
 
@@ -33,20 +37,66 @@ public class APITests {
 
     @Test
     public void registrationTest() {
-        String email = "Guiseppe@reqres.in";
-        String password = "12345678";
+        String email = "eve.holt@reqres.in";
+        String password = "pistol";
         String data = String.format("{ \"email\": \"%s\", \"password\": \"%s\" }", email, password);
-        String data1 = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"pistol\" }";
 
         given()
-                .contentType(ContentType.JSON)
-                .body(data1)
+                .contentType(JSON)
+                .body(data)
                 .when()
                 .log().all()
                 .post("https://reqres.in/api/register")
                 .then()
                 .log().all()
                 .statusCode(200);
+
+    }
+
+
+    @Test
+    public void deleteTest() {
+        int ID = 2;
+        given()
+                .when()
+                .log().all()
+                .delete("https://reqres.in/api/users/" + ID)
+                .then()
+                .log().all()
+                .statusCode(204);
+    }
+
+    @Test
+    public void goodFaviconTest() {
+        given()
+                .when()
+                .log().all()
+                .get("https://www.google.com/favicon.ico")
+                .then()
+                .log().all()
+                .statusCode(200);
+    }
+
+    @Test
+    public void createUserTest() {
+        Faker fake = new Faker();
+        String name = fake.pokemon().name();
+        String job = fake.job().title();
+        String formattedDate = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        String data = String.format("{ \"name\": \"%s\", \"job\": \"%s\" }", name, job);
+
+        given()
+                .when()
+                .log().all()
+                .contentType(JSON)
+                .body(data)
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .body("name",is(name))
+                .body("job",is(job))
+                .body("createdAt",startsWith(formattedDate));
 
     }
 }
